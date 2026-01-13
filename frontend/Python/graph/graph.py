51cef718918432dcdc49be5e7949e80e2aa71002
Mingzhu Yan <yanmingzhu@iscas.ac.cn>
2026-01-13 09:58:17 +0000

    [frontend]: automatically inject profiling instructions in DynamoCompiler
    This commit introduces the `enable_profile` option to automatically measure
    the execution time of each graph node. When enabled, the DynamoCompiler performs
    the following instrumentations after `lower_to_top_level_ir`:
    
    - Inject `rtclock` for timestamp acquire and `record_timing` for data record
    - Injects global strings for each node in format "op_name_{node_index}_{node_name}"
    - Injects timing probes around each node to
       calculate and record the elapsed execution time.

--- frontend/Python/graph/graph.py
+++ frontend/Python/graph/graph.py
@@ -27,6 +27,7 @@ import numpy as np
 
 import buddy_mlir.ir as ir
 import buddy_mlir.dialects.func as func
+from buddy_mlir.dialects import arith, llvm
 from buddy_mlir.passmanager import *
 from buddy_mlir.execution_engine import *
 from buddy_mlir import runtime as rt
@@ -112,6 +113,7 @@ class Graph:
         func_name: str,
         device: DeviceType = DeviceType.CPU,
         verbose=False,
+        enable_profile: bool = False,
         enable_external_calls: bool = False,
     ) -> None:
         """
@@ -137,6 +139,7 @@ class Graph:
         self._imported_module = None
         self._params_ref = None
         self._verbose = verbose
+        self._enable_profile = enable_profile
         self._ops_registry = ops_registry
         self._func_name = func_name
         self._ctx = ir.Context()
@@ -328,7 +331,6 @@ class Graph:
             current node will have this node as their child instead of `node`
         - The last node is taken to be the "tail" of the chain, and all children of `node`
             will have this node as their parent instead.
-
         Args:
             node (Op): The operation to be replaced.
             chain (list[Op]): The a list of nodes to be inserted instead of Op
@@ -491,6 +493,7 @@ class Graph:
                 False,
                 self.device,
                 verbose=self._verbose,
+                enable_profile=self._enable_profile,
                 enable_external_calls=self._enable_external_calls,
             )
             self._imported_module = fx_importer.import_graph()
@@ -615,6 +618,7 @@ class GraphImporter:
         do_param_pack: bool = False,
         device: DeviceType = DeviceType.CPU,
         verbose=False,
+        enable_profile=True,
         enable_external_calls: bool = False,
     ):
         """
@@ -636,6 +640,7 @@ class GraphImporter:
         self._params_shapes = params_shapes
         self._inputs_shapes = inputs_shapes
         self._verbose = verbose
+        self._enable_profile = enable_profile
         self._do_param_pack = do_param_pack
         self._param_packs = []
         self._num_input_visited = 0
@@ -718,7 +723,44 @@ class GraphImporter:
             buddy_mlir.ir.Module: An MLIR module in high-level dialects.
         """
         assert self._do_param_pack == False
+        node_names = [node.name for node in self._body]
         with ir.InsertionPoint(self._module.body):
+            if self._enable_profile:
+                # Add clock func signature
+                f64 = ir.F64Type.get()
+                ptr = llvm.PointerType.get()
+                # func.func private @rtclock() -> f64
+                func.FuncOp(
+                    name="rtclock",
+                    type=ir.FunctionType.get(inputs=[], results=[f64]),
+                    visibility="private",
+                )
+                # func.func private @record_timing(!llvm.ptr, f64)
+                func.FuncOp(
+                    name="record_timing",
+                    type=ir.FunctionType.get(inputs=[ptr, f64], results=[]),
+                    visibility="private",
+                )
+
+                width = len(str(len(node_names)))
+                for i, name in enumerate(node_names):
+                    sym_name = f"op_name_{i:0{width}d}_{name}"
+                    content = sym_name + "\00"
+                    content_type = ir.Type.parse(
+                        f"!llvm.array<{len(content)} x i8>"
+                    )
+                    linkage_attr = ir.Attribute.parse("#llvm.linkage<private>")
+                    llvm.GlobalOp(
+                        content_type,
+                        ir.StringAttr.get(sym_name),
+                        linkage_attr,
+                        constant=True,
+                        value=ir.StringAttr.get(content),
+                        addr_space=ir.IntegerAttr.get(
+                            ir.IntegerType.get_signless(32), 0
+                        ),  # 关键字参数
+                    )
+
             arguments = []
             inputs = self._params_shapes + self._inputs_shapes
             for arg in inputs:
@@ -736,10 +778,21 @@ class GraphImporter:
             @func.FuncOp.from_py_func(*arguments, name=self._func_name)
             def generated_func(*args):
                 args_list = list(args)
-                func_op = self._module.body.operations[0]
-                for node in self._body:
+                func_op_index = (
+                    len(node_names) + 2 if self._enable_profile else 0
+                )
+                func_op = self._module.body.operations[func_op_index]
+                for i, node in enumerate(self._body):
                     if node in extern_func:
                         continue
+
+                    if self._enable_profile:
+                        # %t_start = call @rtclock() : () -> f64
+                        f64_type = ir.F64Type.get()
+                        t_start = func.CallOp(
+                            [f64_type], ir.FlatSymbolRefAttr.get("rtclock"), []
+                        ).results[0]
+
                     old_ops = [op for op in func_op.body.blocks[0].operations]
                     if isinstance(node, OutputOp):
                         output_node_args = node.args
@@ -771,6 +824,31 @@ class GraphImporter:
                             if op not in old_ops:
                                 print(op)
                         print("")
+
+                    if self._enable_profile:
+                        # %t_end = call @rtclock() : () -> f64
+                        t_end = func.CallOp(
+                            [f64_type], ir.FlatSymbolRefAttr.get("rtclock"), []
+                        ).results[0]
+
+                        # %duration = arith.subf %t_end, %t_start : f64
+                        duration = arith.SubFOp(t_end, t_start).result
+
+                        # %name_ptr = llvm.mlir.addressof @op_name_post_attn_layernorm : !llvm.ptr
+                        ptr_type = llvm.PointerType.get()
+                        name_ptr = llvm.AddressOfOp(
+                            ptr_type,
+                            ir.FlatSymbolRefAttr.get(
+                                f"op_name_{i:0{width}d}_{node.name}"
+                            ),
+                        ).result
+
+                        # call @record_timing(%name_ptr, %duration) : (!llvm.ptr, f64) -> ()
+                        func.CallOp(
+                            [],
+                            ir.FlatSymbolRefAttr.get("record_timing"),
+                            [name_ptr, duration],
+                        )
 
                 return self._symbol_table.get(("output", 0))
 
